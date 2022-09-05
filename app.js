19acff0b7e49fd4ba1ad22b012794f43b4048d89
Jose Sandoval <j-os-pablo@hotmail.com>
2022-09-05 15:52:25 -0500

    add cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const ClientRoutes = require('./routes/routes')
 
 const app = express()
+app.use(cors())
 
 // MIDLEWARE
 app.use(express.json())
